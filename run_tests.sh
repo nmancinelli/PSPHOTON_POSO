eed4a69980b08cc3e004cc9371e5635ce2a1f1ed
Nick Mancinelli <mancinelli@sora.local>
2016-05-12 10:38:59 -0400

    Testing the frequency dependence for the assumed heterogeneity model of Kennett and Furumura.

--- run_tests.sh
+++ run_tests.sh
@@ -3,7 +3,7 @@ function write_do_file {
 cat << EOF > do.photon
 ./psphoton << !
 sereno_orcutt
-2.5              !assumed freq. (Hz), affects attenuation and scattering
+$1              !assumed freq. (Hz), affects attenuation and scattering
 35.0             !assumed source depth (km)
 5                !radiate: (1) P,  (2) SH, (3) SV, (4) SH/SV, (5) P+S, (6) custom Es/Ep, (7) custom pmax
 50000            !number of ray parameters for tables (max=50000)
@@ -21,27 +21,27 @@ sereno_orcutt
   5.80 3.36       !reference P & S velocity for layer
   0.8             !relative size of density perturbation (0.8 often assumed)
   0.0             !rms perturbation
-  10.0 0.1        !scale length (km), aspect ratio (az/ax)
+  0.25 20.0        !scale length (km), aspect ratio (az/ax)
     10              !min scat depth (km)
     100             !max scat depth (km)
     999999          !max scat range from source (km)
     8.08 4.47       !reference P & S velocity for layer
     0.8             !relative size of density perturbation (0.8 often assumed)
-    $1              !rms perturbation
-    10.0 0.1        !scale length (km), aspect ratio (az/ax)
+    2.0              !rms perturbation
+    10.0 0.05        !scale length (km), aspect ratio (az/ax)
   100             !min scat depth (km)
   300             !max scat depth (km)
   999999          !max scat range from source (km)
   8.08 4.47       !reference P & S velocity for layer
   0.8             !relative size of density perturbation (0.8 often assumed)
-  $1              !rms perturbation
-  10.0 0.1        !scale length (km), aspect ratio (az/ax)
+  2.0              !rms perturbation
+  5.0 0.2        !scale length (km), aspect ratio (az/ax)
 	300              !min scat depth (km)
 	9999             !max scat depth (km)
 	999999          !max scat range from source (km)
 	8.08 4.47       !reference P & S velocity for layer
 	0.8             !relative size of density perturbation (0.8 often assumed)
-	$1              !rms perturbation
+	0.0              !rms perturbation
 	10.0 0.1        !scale length (km), aspect ratio (az/ax)
 0 99999          !min,max number of scattering events for output
 5                !number of intrinsic Q layers (to follow) (max=6)
@@ -62,25 +62,25 @@ EOF
 #
 cd TEST0
 rm out.debug
-write_do_file 0.00
+write_do_file 2.00
 bash do.photon > tmp &
 cd -
 #
 cd TEST1
 rm out.debug
-write_do_file 0.02
+write_do_file 4.00
 bash do.photon > tmp &
 cd -
 #
 cd TEST2
 rm out.debug
-write_do_file 0.04
+write_do_file 8.00
 bash do.photon > tmp &
 cd -
 #
 cd TEST3
 rm out.debug
-write_do_file 0.06
+write_do_file 16.00
 bash do.photon > tmp &
 cd -
 echo "  "
